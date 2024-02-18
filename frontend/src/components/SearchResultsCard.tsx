002e03652d8cdec2f7884fe6037910c643512664
TigMart <tig.mar@yandex.ru>
2024-02-18 09:48:01 -0800

    search page draft

---
+++ frontend/src/components/SearchResultsCard.tsx
@@ -0,0 +1,21 @@
+import { HotelType } from "../../../backend/src/shared/types";
+
+type Props = {
+  hotel: HotelType;
+};
+
+const SearchResultCard = ({ hotel }: Props) => {
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-[2fr_3fr] border border-slate-300 rounded-lg p-8 gap-8">
+      <div className="w-full h-[300px]">
+        <img
+          src={hotel.imageUrls[0]}
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      <div className="flex flex-col justify-between">details columns</div>
+    </div>
+  );
+};
+
+export default SearchResultCard;
